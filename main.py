e632aa408d78f6b26c5b7b201d5f6ede5ff552d8
fajrinmk <fajrinmk@gmail.com>
2018-11-19 10:42:09 +0700

    siapkan penanda kapal dan lawan untuk interface terminak=l

---
+++ main.py
@@ -0,0 +1,14 @@
+def main():
+    global SHIP, HIT, MISS, EMPTY
+    global TOP, SIDE
+    global ships
+    
+    #Set the ship for the terminal interface
+
+    SHIP = "S"
+    HIT = "X"
+    MISS = "O"
+    EMPTY = " "
+
+    TOP = "ABCDEFGHIJ"
+    SIDE = "0123456789"
