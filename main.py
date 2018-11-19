3c558dd8c2d8692649c7fa5a6dc265a961884cf3
fajrinmk <fajrinmk@gmail.com>
2018-11-19 10:43:23 +0700

    siapkan banyaknya kapal yg tersedia

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ def main():
     global TOP, SIDE
     global ships
     
-    #Set the ship for the terminal interface
-
+    # Set the ship for the terminal interface
     SHIP = "S"
     HIT = "X"
     MISS = "O"
@@ -12,3 +11,6 @@ def main():
 
     TOP = "ABCDEFGHIJ"
     SIDE = "0123456789"
+
+    # Available ships for the game
+    ships = [["Carrier", 5, 1], ["Battleships", 4, 2], ["Cruisers", 3, 2], ["Submarines", 2, 1]]
